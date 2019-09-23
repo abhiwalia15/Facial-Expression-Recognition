9925fa918a81b71b1705fd28131f59a3d4a5692c
abhiwalia15 <waliamrinal@gmail.com>
2019-09-23 15:52:50 +0530

    adding the artist name, id, track number, uri and album name

--- artist_name.py
+++ artist_name.py
@@ -10,7 +10,7 @@ client_secret = '578a8282d52541e1acc2087e024d8698'
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spotify object to access API
 
-name = "Coldplay" #chosen artist
+name = "Imagine Dragons" #chosen artist
 
 result = sp.search(name) #search query
 #print(result['tracks']['items'][0]['artists'])
@@ -33,3 +33,34 @@ for i in range(len(sp_albums['items'])):
 print(album_names[0])
 print(album_uris[0])
 #Keep names and uris in same order to keep track of duplicate albums
+
+
+def albumSongs(uri):
+    album = uri #assign album uri to a_name
+    spotify_albums[album] = {} #Creates dictionary for that specific album
+
+    #Create keys-values of empty lists inside nested dictionary for album
+    spotify_albums[album]['album'] = [] #create empty list
+    spotify_albums[album]['track_number'] = []
+    spotify_albums[album]['id'] = []
+    spotify_albums[album]['name'] = []
+    spotify_albums[album]['uri'] = []
+
+    tracks = sp.album_tracks(album) #pull data on album tracks
+
+    for n in range(len(tracks['items'])): #for each song track
+        spotify_albums[album]['album'].append(album_names[album_count]) #append album name tracked via album_count
+        spotify_albums[album]['track_number'].append(tracks['items'][n]['track_number'])
+        spotify_albums[album]['id'].append(tracks['items'][n]['id'])
+        spotify_albums[album]['name'].append(tracks['items'][n]['name'])
+        spotify_albums[album]['uri'].append(tracks['items'][n]['uri'])
+
+
+spotify_albums = {}
+album_count = 0
+for i in album_uris: #each album
+    albumSongs(i)
+    print("Album " + str(album_names[album_count]) + " songs has been added to spotify_albums dictionary")
+    album_count+=1 #Updates album count once all tracks have been added
+
+print(spotify_albums['name'][0])
