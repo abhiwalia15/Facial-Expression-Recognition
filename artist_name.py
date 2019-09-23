3676e17318a5e01cdd788c646a1c104bb3a14cd6
abhiwalia15 <waliamrinal@gmail.com>
2019-09-23 15:41:25 +0530

    added albums name and artist name

--- artist_name.py
+++ artist_name.py
@@ -9,6 +9,27 @@ client_secret = '578a8282d52541e1acc2087e024d8698'
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spotify object to access API
+
 name = "Coldplay" #chosen artist
+
 result = sp.search(name) #search query
-print(result['tracks']['items'][0]['artists'])
+#print(result['tracks']['items'][0]['artists'])
+
+#Extract Artist's uri
+artist_uri = result['tracks']['items'][0]['artists'][0]['uri']
+
+#Pull all of the artist's albums
+sp_albums = sp.artist_albums(artist_uri, album_type='album')
+
+#Store artist's albums' names' and uris in separate lists
+
+album_names = []
+album_uris = []
+
+for i in range(len(sp_albums['items'])):
+    album_names.append(sp_albums['items'][i]['name'])
+    album_uris.append(sp_albums['items'][i]['uri'])
+    
+print(album_names[0])
+print(album_uris[0])
+#Keep names and uris in same order to keep track of duplicate albums
