7c08f804b887436487b3f571bc59423dae655c45
abhiwalia15 <waliamrinal@gmail.com>
2019-10-01 17:43:54 +0530

    test database created

--- DataBase.py
+++ DataBase.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-con = sqlite3.connect('testfile.db')
-print("OPENED")
+# con = sqlite3.connect('testfile.db')
+# print("OPENED")
 
-tables = "create table tab (song_names string, uri text, movies text)"
+# tables = "create table tab (song_names string, uri text, movies text)"
 
-print("Table created")
+# print("Table created")
 
-tab = "insert into tab (song_names , uri , movies ) values ('bad lier', '1135g1g1fs1', 'project x')"
-con.execute(tab)
+# tab = "insert into tab (song_names , uri , movies ) values ('bad lier', '1135g1g1fs1', 'project x')"
+# con.execute(tab)
