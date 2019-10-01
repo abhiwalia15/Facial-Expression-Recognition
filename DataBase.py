7bd0b0fee0f293c49ede8fea4569b1e8a7449e4f
abhiwalia15 <waliamrinal@gmail.com>
2019-10-01 17:16:49 +0530

    test database created

--- DataBase.py
+++ DataBase.py
@@ -2,3 +2,8 @@ import sqlite3
 
 con = sqlite3.connect('testfile.db')
 print("OPENED")
+
+table = "create table tab (song_names string, uri text, movies text)"
+
+con.execute(table)
+print("Table created")
