0b3aea634ae207fcb3b8500612cea76405e4f999
abhiwalia15 <waliamrinal@gmail.com>
2019-10-01 17:55:21 +0530

    test database created

--- DataBase.py
+++ DataBase.py
@@ -15,3 +15,24 @@ import sqlite3
 # c = con.cursor()
 
 # c.execute("insert into list (songs, uri, movies) values(?,?,?)", ('asd','agh','aaa'))
+
+
+import sqlite3 
+  
+conn = sqlite3.connect('pythonDB.db') 
+c = conn.cursor() 
+  
+def create_table(): 
+    c.execute('CREATE TABLE IF NOT EXISTS RecordONE (Number REAL, Name TEXT)') 
+  
+def data_entry(): 
+    number = 1234
+    name = "GeeksforGeeks"
+    c.execute("INSERT INTO RecordONE (Number, Name) VALUES(?, ?)", (number, name)) 
+    conn.commit() 
+  
+create_table() 
+data_entry() 
+  
+c.close() 
+conn.close() 
