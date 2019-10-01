31f5fd8d0ba8fe62f2849a7f321b9707144405a5
abhiwalia15 <waliamrinal@gmail.com>
2019-10-01 17:57:29 +0530

    test database created

--- DataBase.py
+++ DataBase.py
@@ -10,11 +10,18 @@ import sqlite3
 # tab = "insert into tab (song_names , uri , movies ) values ('bad lier', '1135g1g1fs1', 'project x')"
 # con.execute(tab)
 
+# **************************************************************
+# *************************************************
 
 # con = sqlite3.connect('test.db')
 # c = con.cursor()
 
 # c.execute("insert into list (songs, uri, movies) values(?,?,?)", ('asd','agh','aaa'))
+
+
+# ******************************************************************8888
+# ******************************************************
+
 
 
 import sqlite3 
@@ -36,3 +43,33 @@ data_entry()
   
 c.close() 
 conn.close() 
+
+# ********************************************************************
+# **************************************************************
+
+
+import sqlite3 
+  
+conn = sqlite3.connect('pythonDB.db') 
+c = conn.cursor() 
+  
+def create_table(): 
+    c.execute('CREATE TABLE IF NOT EXISTS RecordONE (Number REAL, Name TEXT)') 
+  
+def data_entry(): 
+    number = 1234
+    name = "GeeksforGeeks"
+    c.execute("INSERT INTO RecordONE (Number, Name) VALUES(?, ?)", (number, name)) 
+    conn.commit() 
+  
+create_table() 
+data_entry() 
+  
+c.close() 
+conn.close() 
+
+
+
+# ********************************************************************
+# **************************************************************
+
